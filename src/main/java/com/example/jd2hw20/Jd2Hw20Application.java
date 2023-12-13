e90386cc8c99840a7e9e1ebd84b88e655e2940ed
Nikita Kovalov <nik.kovalev1760@gmail.com>
2023-12-13 07:40:20 -0500

    Initial commit

---
+++ src/main/java/com/example/jd2hw20/Jd2Hw20Application.java
@@ -0,0 +1,13 @@
+package com.example.jd2hw20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jd2Hw20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jd2Hw20Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jd2hw20/Jd2Hw20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jd2hw20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jd2Hw20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
